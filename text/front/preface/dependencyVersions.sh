84342f8bd4db6f8b765cb683fc52df4befbc48f1
Thomas Weise <tweise@hfuu.edu.cn>
2025-01-26 10:19:54 +0800

    added images for LibreOffice installation

--- text/front/preface/dependencyVersions.sh
+++ text/front/preface/dependencyVersions.sh
@@ -15,24 +15,43 @@ hasOutput=false  # Do we have some output and need a separator?
 postgresVersion="$(psql --version 2>/dev/null | grep Postgre | sed -n 's/\psql (PostgreSQL) \([.0-9]*\)/\1/p')" || true
 postgresVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${postgresVersion}")" || true
 if [ -n "$postgresVersion" ]; then
-    echo "postgres-client: $postgresVersion"
+    echo "PostgreSQL client: $postgresVersion"
     hasOutput=true
 fi
 
 # Find the Python interpreter.
 if [[ $(declare -p POSTGRES_USER 2>/dev/null) == declare\ ?x* ]]; then
-  if [[ $(declare -p POSTGRES_PASSWORD 2>/dev/null) == declare\ ?x* ]]; then
-    if [[ $(declare -p POSTGRES_HOST 2>/dev/null) == declare\ ?x* ]]; then
-      if [[ $(declare -p POSTGRES_PORT 2>/dev/null) == declare\ ?x* ]]; then
-        postgresServerVersion="$(psql "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}" -c "show server_version;" 2>/dev/null | grep "Ubuntu")" || true
-        postgresServerVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${postgresServerVersion}")" || true
-        if [ -n "$postgresServerVersion" ]; then
-            echo "postgres-server: $postgresServerVersion"
-            hasOutput=true
-        fi
-      fi
-    fi
-  fi
+  user="${POSTGRES_USER}"
+else
+  user="postgres"
+fi
+if [[ $(declare -p POSTGRES_PASSWORD 2>/dev/null) == declare\ ?x* ]]; then
+  password=":${POSTGRES_PASSWORD}"
+else
+  password=""
+fi
+if [[ $(declare -p POSTGRES_HOST 2>/dev/null) == declare\ ?x* ]]; then
+  host="${POSTGRES_HOST}"
+else
+  host="localhost"
+fi
+if [[ $(declare -p POSTGRES_PORT 2>/dev/null) == declare\ ?x* ]]; then
+  port="${POSTGRES_PORT}"
+else
+  port="5432"
+fi
+postgresServerVersion="$(psql "postgres://${user}:${password}@${host}:${port}" -wc "show server_version;" 2>/dev/null | grep "Ubuntu")" || true
+postgresServerVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${postgresServerVersion}")" || true
+if [ -n "$postgresServerVersion" ]; then
+    echo "PostgreSQL server: $postgresServerVersion"
+    hasOutput=true
+fi
+
+libreOfficeVersion="$(libreoffice --version 2>/dev/null | grep "LibreOffice" | sed -n 's/LibreOffice \([.0-9]*\)/\1/p')" || true
+libreOfficeVersion="$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' -e 's/[[:space:]][[:space:]][[:space:]]*/ /g' -e 's/\.*$//'<<<"${libreOfficeVersion}")" || true
+if [ -n "$libreOfficeVersion" ]; then
+    echo "LibreOffice: $libreOfficeVersion"
+    hasOutput=true
 fi
 
 # separator if we have anything
